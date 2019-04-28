b964cd2ce9993847e3dac3aa82a1fb09ee7b2e99
Sophia Brandt <tech@s-br.net>
2019-04-28 12:33:45 +0200

    📦 NEW: create HomePage as functional component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React from 'react'
+import NewMarket from '../components/NewMarket'
+import MarketList from '../components/MarketList'
 
-class HomePage extends React.Component {
-  state = {};
-
-  render() {
-    return <div>Home</div>;
-  }
+function HomePage() {
+  return (
+    <>
+      <NewMarket />
+      <MarketList />
+    </>
+  )
 }
 
-export default HomePage;
+export default HomePage
