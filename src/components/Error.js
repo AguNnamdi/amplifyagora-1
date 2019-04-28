ca0419c089c6da3c3e435a76d2d5726d1214402e
Sophia Brandt <tech@s-br.net>
2019-04-28 18:28:14 +0200

    📦 NEW: add Error component

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,5 +1,11 @@
-import React from "react";
+import React from 'react'
 
-const Error = () => <div>Error</div>;
+const Error = ({ errors }) => (
+  <pre className="error">
+    {errors.map(({ message }, index) => (
+      <div key={index}>{message}</div>
+    ))}
+  </pre>
+)
 
-export default Error;
+export default Error
