dcddda59813f48cfc80cf0182023aa85e3d155cd
Sophia Brandt <tech@s-br.net>
2019-04-30 13:07:59 +0200

    👌 IMPROVE: refactor MarketPage to functional component

--- src/pages/MarketPage.js
+++ src/pages/MarketPage.js
@@ -1,12 +1,8 @@
 import React from 'react'
 // import { Loading, Tabs, Icon } from "element-react";
 
-class MarketPage extends React.Component {
-  state = {}
-
-  render() {
-    return <div>MarketPage {this.props.marketId}</div>
-  }
+const MarketPage = ({ marketId }) => {
+  return <div>MarketPage {marketId}</div>
 }
 
 export default MarketPage
