02c1de2c0b4bea56e761b9321d8c0911e8f32e7a
Sophia Brandt <tech@s-br.net>
2019-05-01 12:23:41 +0200

    👌 IMPROVE: remove Error from MarketPage

--- src/pages/MarketPage.js
+++ src/pages/MarketPage.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Loading, Tabs, Icon } from 'element-react'
-import Error from '../components/Error'
 import useFetchMarketData from '../components/helpers/useFetchMarketData'
 import NewProduct from '../components/NewProduct'
 import Product from '../components/Product'
 
 const MarketPage = ({ marketId, user }) => {
-  const { market, isMarketOwner, isLoading, isError } = useFetchMarketData({
+  const { market, isMarketOwner, isLoading } = useFetchMarketData({
     marketId,
     user,
   })
 
-  return isLoading && !isError ? (
+  return isLoading ? (
     <Loading fullscreen={true} />
   ) : (
     <>
