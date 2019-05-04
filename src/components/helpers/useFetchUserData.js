0dec58e13429307b4115d782b481b863714f4f64
Sophia Brandt <tech@s-br.net>
2019-05-04 21:42:45 +0200

    👌 IMPROVE: fix re-render trigger for fetchUserData

--- src/components/helpers/useFetchUserData.js
+++ src/components/helpers/useFetchUserData.js
@@ -3,65 +3,64 @@ import { Auth, Hub } from 'aws-amplify'
 
 const useFetchUserData = () => {
   const [user, setUser] = useState(null)
+  const [isSignedIn, setIsSignedIn] = useState(false)
 
   useEffect(() => {
-    let didCancel = false
+    let mounted = true
 
     const fetchUserData = async () => {
       try {
         const data = await Auth.currentAuthenticatedUser()
-        if (!didCancel) {
+        if (mounted) {
           data ? setUser(data) : setUser(null)
         }
       } catch (error) {
-        if (!didCancel) {
-          console.error(error)
-        }
+        console.error(error)
       }
     }
-    fetchUserData()
-    return () => {
-      didCancel = true
-    }
-  }, [])
-
-  useEffect(() => {
-    let didCancel = true
 
     const HubListener = () => {
       Hub.listen('auth', data => {
         const { payload } = data
-        if (!didCancel) {
-          onAuthEvent(payload)
-        }
+        onAuthEvent(payload)
       })
     }
+
+    const onAuthEvent = payload => {
+      switch (payload.event) {
+        case 'signIn':
+          if (mounted) {
+            setIsSignedIn(true)
+            console.log('signed in')
+          }
+          break
+        case 'signup':
+          console.log('signed up')
+          break
+        case 'signout':
+          if (mounted) {
+            setIsSignedIn(false)
+            console.log('signed out')
+          }
+          break
+        default:
+          return
+      }
+    }
+
     HubListener()
+    fetchUserData()
+
     return () => {
       Hub.remove('auth')
-      didCancel = true
+      mounted = false
     }
-  }, [])
-
-  const onAuthEvent = payload => {
-    switch (payload.event) {
-      case 'signIn':
-        console.log('signed in')
-        break
-      case 'signup':
-        console.log('signed up')
-        break
-      case 'signout':
-        console.log('signed out')
-        break
-      default:
-        return
-    }
-  }
+  }, [isSignedIn])
 
   const handleSignout = async () => {
     try {
       await Auth.signOut()
+      setIsSignedIn(false)
       setUser(null)
     } catch (error) {
       console.error('Error signing out user ', error)
