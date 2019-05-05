fd0bd50cb6947df7a325acca0a07d6ac67cd388d
Sophia Brandt <tech@s-br.net>
2019-05-05 21:42:07 +0200

    📦 NEW: add subscriptions to product changes in hook

--- src/components/helpers/useFetchMarketData.js
+++ src/components/helpers/useFetchMarketData.js
@@ -1,24 +1,29 @@
 import { useEffect, useReducer } from 'react'
 import { API, graphqlOperation } from 'aws-amplify'
 import { getMarket } from '../../graphql/queries'
+import {
+  onCreateProduct,
+  onUpdateProduct,
+  onDeleteProduct,
+} from '../../graphql/subscriptions'
 
 const useFetchMarketData = ({ marketId, user }) => {
-  const [state, dispatch] = useReducer(marketFetchReducer, {
+  const [state, dispatch] = useReducer(reducer, {
     isLoading: true,
     isError: false,
-    market: null,
+    market: {},
     isMarketOwner: false,
   })
 
-  function marketFetchReducer(state, action) {
+  function reducer(state, action) {
     switch (action.type) {
-      case 'FETCH_INIT':
+      case 'FETCH_MARKET_INIT':
         return {
           ...state,
           isLoading: true,
           isError: false,
         }
-      case 'FETCH_SUCCESS':
+      case 'FETCH_MARKET_SUCCESS':
         return {
           ...state,
           isLoading: false,
@@ -26,11 +31,16 @@ const useFetchMarketData = ({ marketId, user }) => {
           market: action.payload,
           isMarketOwner: user.username === action.payload.owner,
         }
-      case 'FETCH_FAILURE':
+      case 'FETCH_MARKET_FAILURE':
         return {
           ...state,
           isLoading: false,
           isError: true,
+        }
+      case 'FETCH_PRODUCTS':
+        return {
+          ...state,
+          market: action.payload,
         }
       default:
         throw new Error()
@@ -38,30 +48,87 @@ const useFetchMarketData = ({ marketId, user }) => {
   }
 
   useEffect(() => {
-    let didCancel = false
+    let mounted = true
 
     const fetchMarket = async () => {
-      dispatch({ type: 'FETCH_INIT' })
+      if (mounted) {
+        dispatch({ type: 'FETCH_MARKET_INIT' })
+      }
       try {
-        const input = { id: marketId }
-        const result = await API.graphql(graphqlOperation(getMarket, input))
-        if (!didCancel) {
+        if (mounted) {
+          const input = { id: marketId }
+          const result = await API.graphql(graphqlOperation(getMarket, input))
           dispatch({
-            type: 'FETCH_SUCCESS',
+            type: 'FETCH_MARKET_SUCCESS',
             payload: result.data.getMarket,
           })
         }
       } catch (error) {
-        if (!didCancel) {
-          dispatch({ type: 'FETCH_FAILURE' })
+        if (mounted) {
+          dispatch({ type: 'FETCH_MARKET_FAILURE' })
         }
       }
     }
+
     fetchMarket()
+
+    return () => (mounted = false)
+  }, [marketId])
+
+  useEffect(() => {
+    const createProductListener = API.graphql(
+      graphqlOperation(onCreateProduct)
+    ).subscribe({
+      next: productData => {
+        const createdItem = productData.value.data.onCreateProduct
+        const prevItems = state.market.products.items.filter(
+          item => item.id !== createdItem.id
+        )
+        const updatedItems = [createdItem, ...prevItems]
+        let updatedMarket = { ...state.market }
+        updatedMarket.products.items = updatedItems
+        dispatch({ type: 'FETCH_PRODUCTS', payload: updatedMarket })
+      },
+    })
+
+    const updateProductListener = API.graphql(
+      graphqlOperation(onUpdateProduct)
+    ).subscribe({
+      next: productData => {
+        const updatedItem = productData.value.data.onUpdateProduct
+        const updatedItemIndex = state.market.products.items.findIndex(
+          item => item.id === updatedItem.id
+        )
+        const updatedItems = [
+          ...state.market.products.items.slice(0, updatedItemIndex),
+          updatedItem,
+          ...state.market.products.items.slice(updatedItemIndex + 1),
+        ]
+        let updatedMarket = { ...state.market }
+        updatedMarket.products.items = updatedItems
+        dispatch({ type: 'FETCH_PRODUCTS', payload: updatedMarket })
+      },
+    })
+
+    const deleteProductListener = API.graphql(
+      graphqlOperation(onDeleteProduct)
+    ).subscribe({
+      next: productData => {
+        const deletedItem = productData.value.data.onDeleteProduct
+        const updatedItems = state.market.products.items.filter(
+          item => item.id !== deletedItem.id
+        )
+        let updatedMarket = { ...state.market }
+        updatedMarket.products.items = updatedItems
+        dispatch({ type: 'FETCH_PRODUCTS', payload: updatedMarket })
+      },
+    })
     return () => {
-      didCancel = true
+      createProductListener.unsubscribe()
+      updateProductListener.unsubscribe()
+      deleteProductListener.unsubscribe()
     }
-  }, [dispatch, marketId])
+  }, [state])
 
   return { ...state }
 }
