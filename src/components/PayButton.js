3031e5127372a7a6a5eb9dbfc748b3e7640fe010
Sophia Brandt <tech@s-br.net>
2019-06-07 17:22:15 +0200

    📦 NEW: add handleCharge

--- src/components/PayButton.js
+++ src/components/PayButton.js
@@ -1,6 +1,6 @@
 import React from 'react'
+import { API, graphqlOperation } from 'aws-amplify'
 import StripeCheckout from 'react-stripe-checkout'
-// import { Notification, Message } from "element-react";
 
 const stripeConfig = {
   currency: 'USD',
@@ -8,8 +8,27 @@ const stripeConfig = {
 }
 
 const PayButton = ({ product, user }) => {
+  const handleCharge = async token => {
+    try {
+      const result = await API.post('orderlambda', '/charge', {
+        body: {
+          token,
+          charge: {
+            currency: stripeConfig.currency,
+            amount: product.price,
+            description: product.description,
+          },
+        },
+      })
+      console.log({ result })
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   return (
     <StripeCheckout
+      token={handleCharge}
       email={user.attributes.email}
       name={product.description}
       amount={product.price}
